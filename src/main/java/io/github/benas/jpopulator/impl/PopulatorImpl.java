1e0e12b8d0b19a23590990cffce7c9eb5f775106
benas <md.benhassine@gmail.com>
2013-12-11 22:35:48 +0100

    rename base package from net.benas to io.github.benas

---
+++ src/main/java/io/github/benas/jpopulator/impl/PopulatorImpl.java
@@ -0,0 +1,238 @@
+/*
+ * The MIT License
+ *
+ *   Copyright (c) 2013, benas (md.benhassine@gmail.com)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package io.github.benas.jpopulator.impl;
+
+import io.github.benas.jpopulator.api.Randomizer;
+import io.github.benas.jpopulator.api.Populator;
+import org.apache.commons.beanutils.PropertyUtils;
+
+import java.lang.reflect.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The core implementation of the {@link Populator} interface.
+ *
+ * @author benas (md.benhassine@gmail.com)
+ */
+public class PopulatorImpl implements Populator {
+
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
+    /**
+     * Custom randomizers map to use to generate random values.
+     */
+    private Map<RandomizerDefinition, Randomizer> randomizers;
+
+    /**
+     * The supported java types list.
+     */
+    private final List<Class> javaTypesList;
+
+    public PopulatorImpl() {
+
+        randomizers = new HashMap<RandomizerDefinition, Randomizer>();
+        javaTypesList = new ArrayList<Class>();
+
+        //initialize supported java types
+        Class[] javaTypes = {String.class, Character.TYPE, Character.class,
+                Boolean.TYPE, Boolean.class,
+                Byte.TYPE, Byte.class, Short.TYPE, Short.class, Integer.TYPE, Integer.class, Long.TYPE, Long.class,
+                Double.TYPE, Double.class, Float.TYPE, Float.class, BigInteger.class, BigDecimal.class,
+                AtomicLong.class, AtomicInteger.class,
+                java.util.Date.class, java.sql.Date.class, java.sql.Time.class, java.sql.Timestamp.class, Calendar.class};
+        javaTypesList.addAll(Arrays.asList(javaTypes));
+
+    }
+
+    @Override
+    public Object populateBean(Class type) {
+
+        Object result;
+        try {
+
+            /*
+             * For enum types, no instantiation needed (else java.lang.InstantiationException)
+             */
+            if (type.isEnum()) {
+                return DefaultRandomizer.getRandomValue(type);
+            }
+
+            /*
+             * Create an instance of the type
+             */
+            result = type.newInstance();
+
+            /*
+             * Retrieve declared fields
+             */
+            List<Field> declaredFields = new ArrayList<Field>(Arrays.asList(result.getClass().getDeclaredFields()));
+
+            /*
+             * Retrieve inherited fields for all type hierarchy
+             */
+            Class clazz = type;
+            while (clazz.getSuperclass() != null) {
+                Class superclass = clazz.getSuperclass();
+                declaredFields.addAll(Arrays.asList(superclass.getDeclaredFields()));
+                clazz = superclass;
+            }
+
+            /*
+             * Generate random data for each field
+             */
+            for (Field field : declaredFields) {
+                if (isCollectionType(field.getType())) {
+                    populateCollectionType(result, field);
+                } else {
+                    populateSimpleType(result, field);
+                }
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Unable to populate an instance of type " + type, e);
+            return null;
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<Object> populateBeans(Class type) {
+        byte size = (byte) Math.abs((Byte) DefaultRandomizer.getRandomValue(Byte.TYPE));
+        return populateBeans(type, size);
+    }
+
+    @Override
+    public List<Object> populateBeans(Class type, int size) {
+        Object[] beans = new Object[size];
+        for (int i = 0; i < size; i++) {
+            Object bean = populateBeans(type);
+            beans[i] = bean;
+        }
+        return Arrays.asList(beans);
+    }
+
+    /**
+     * Method to populate a simple (ie non collection) type which can be a java built-in type or a user's custom type.
+     *
+     * @param result The result object on which the generated value will be set
+     * @param field  The field in which the generated value will be set
+     * @throws Exception Thrown when the generated value cannot be set to the given field
+     */
+    private void populateSimpleType(Object result, Field field) throws Exception {
+
+        Class<?> fieldType = field.getType();
+        String fieldName = field.getName();
+        Class<?> resultClass = result.getClass();
+
+        Object object;
+        if (customRandomizer(resultClass, fieldType, fieldName)) { // use custom randomizer if any
+            object = randomizers.get(new RandomizerDefinition(resultClass, fieldType, fieldName)).getRandomValue();
+        } else if (isJavaType(fieldType)) { //Java type (no need for recursion)
+            object = DefaultRandomizer.getRandomValue(fieldType);
+        } else { // Custom type (recursion needed to populate nested custom types if any)
+            object = populateBean(fieldType);
+        }
+        PropertyUtils.setProperty(result, fieldName, object);
+
+    }
+
+    /**
+     * This methods checks if the user has registered a custom randomizer for the given type and field.
+     *
+     * @param type      The class type for which the method should check if a custom randomizer is registered
+     * @param fieldType the field type within the class for which the method should check if a custom randomizer is registered
+     * @param fieldName the field name within the class for which the method should check if a custom randomizer is registered
+     * @return True if a custom randomizer is registered for the given type and field, false else
+     */
+    private boolean customRandomizer(Class<?> type, Class<?> fieldType, String fieldName) {
+        return randomizers.get(new RandomizerDefinition(type, fieldType, fieldName)) != null;
+    }
+
+    /**
+     * Method to populate a collection type which can be a array or a {@link Collection}.
+     *
+     * @param result The result object on which the generated value will be set
+     * @param field  The field in which the generated value will be set
+     * @throws Exception Thrown when the generated value cannot be set to the given field
+     */
+    private void populateCollectionType(Object result, Field field) throws Exception {
+
+        Class<?> fieldType = field.getType();
+        String fieldName = field.getName();
+
+        //Array type
+        if (fieldType.isArray()) {
+            PropertyUtils.setProperty(result, fieldName, Array.newInstance(fieldType.getComponentType(), 0));
+            return;
+        }
+
+        //Collection type
+        Object collection = null;
+        if (List.class.isAssignableFrom(fieldType)) { // List, ArrayList, LinkedList, etc
+            collection = Collections.emptyList();
+        } else if (Set.class.isAssignableFrom(fieldType)) { // Set, HashSet, TreeSet, LinkedHashSet, etc
+            collection = Collections.emptySet();
+        } else if (Map.class.isAssignableFrom(fieldType)) { // Map, HashMap, Dictionary, Properties, etc
+            collection = Collections.emptyMap();
+        }
+        PropertyUtils.setProperty(result, fieldName, collection);
+
+    }
+
+    /**
+     * This method checks if the given type is a java built-in (primitive/boxed) type (ie, int, long, etc)
+     *
+     * @param type the type that the method should check
+     * @return true if the given type is a java built-in type
+     */
+    private boolean isJavaType(Class<?> type) {
+        return javaTypesList.contains(type);
+    }
+
+    /**
+     * This method checks if the given type is a java built-in collection type (ie, array, List, Set, Map, etc)
+     *
+     * @param type the type that the method should check
+     * @return true if the given type is a java built-in collection type
+     */
+    private boolean isCollectionType(Class<?> type) {
+        return type.isArray() || Map.class.isAssignableFrom(type) || Collection.class.isAssignableFrom(type);
+    }
+
+    /**
+     * Setter for the custom randomizers to use. Used to register custom randomizers by the {@link PopulatorBuilder}
+     *
+     * @param randomizers the custom randomizers to use.
+     */
+    public void setRandomizers(Map<RandomizerDefinition, Randomizer> randomizers) {
+        this.randomizers = randomizers;
+    }
+}
---
+++ src/test/java/io/github/benas/jpopulator/beans/Street.java
@@ -0,0 +1,82 @@
+/*
+ * The MIT License
+ *
+ *   Copyright (c) 2013, benas (md.benhassine@gmail.com)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package io.github.benas.jpopulator.beans;
+
+/**
+ * A java bean for the Street type.
+ *
+ * @author benas (md.benhassine@gmail.com)
+ */
+public class Street {
+
+    private int number;
+
+    private byte type;
+
+    private String name;
+
+    public Street() {
+    }
+
+    public Street(int number, byte type, String name) {
+        this.number = number;
+        this.type = type;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Street{");
+        sb.append("number=").append(number);
+        sb.append(", type=").append(type);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
