05b2739df41b11bb5b177d53803a4dd94489f41d
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-01-29 07:44:06 +0100

    add showcase for issue #19

--- src/test/java/io/github/benas/jpopulator/impl/PopulatorTest.java
+++ src/test/java/io/github/benas/jpopulator/impl/PopulatorTest.java
@@ -28,12 +28,17 @@ import io.github.benas.jpopulator.api.Populator;
 import io.github.benas.jpopulator.beans.Address;
 import io.github.benas.jpopulator.beans.Gender;
 import io.github.benas.jpopulator.beans.Person;
+import io.github.benas.jpopulator.beans.SocialPerson;
 import io.github.benas.jpopulator.randomizers.CityRandomizer;
 import io.github.benas.jpopulator.randomizers.EmailRandomizer;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -198,6 +203,48 @@ public class PopulatorTest {
 
         Assert.assertNotNull(person.getAddress().getZipCode());
         Assert.assertFalse(person.getAddress().getZipCode().isEmpty());
+    }
+
+    @Test
+    public void testParametrizedCollectionTypeInference() {
+        // Note: error handling will be added when the feature is implemented
+
+        //Get declared fields
+        Field[] fields = SocialPerson.class.getDeclaredFields();
+
+        // Get the "friends" field which is of type java.util.Set<Person>
+        Field friendsField = fields[0];
+        System.out.println("friendsField = " + friendsField.getName());
+
+        /*
+         * Now the goal is to be able introspect that the actual type of objects in the Set is Person.class
+         */
+
+        // Get the generic type of the friends field
+        Type genericType = friendsField.getGenericType();// java.util.Set<Person>
+        System.out.println("genericType = " + genericType);
+
+        /*
+         * At this point, at runtime, we would like to know if the Set is parametrized or not:
+         *  - If it is parametrized (ie genericType instanceOf ParameterizedType), then jPopulator should be able to get the
+         *      actual type, generate random instances and fill the Set
+         *  - If the set is not parametrized, jPopulator is not able to know which type of objects to generate,
+         *      hence, it will generate an empty collections (but.. in this case, are there folks using non-typed collections in 2015?
+         *      using collections with raw types is a bad practice, cf effective java 2nd edition - Item 23)
+         */
+
+        // Get the parametrized type of the friends field
+        ParameterizedType parameterizedType = (ParameterizedType) genericType; // the test "genericType instanceOf ParameterizedType" will be added in the implementation
+        System.out.println("parameterizedType = " + parameterizedType);
+
+        // Get the actual types (this is an array because there could be multiple types, think of MyType<I, O, R> for example)
+        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+
+        // Get the actual type
+        Type actualTypeArgument = actualTypeArguments[0];// Person.class
+        System.out.println("actualTypeArgument = " + actualTypeArgument);
+
+        Assert.assertEquals(actualTypeArgument, Person.class);
     }
 
 }
