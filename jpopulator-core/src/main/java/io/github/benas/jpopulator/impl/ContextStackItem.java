de89be2fd143bbf2efe0bf99a48194c9e8e03d75
Toilal <toilal.dev@gmail.com>
2015-08-12 17:21:59 +0200

    Add populator recursion context
    This adds support for recursive bean references.
    
    It also adds support for deep ignore with dotted notation.
    
    Close #24

---
+++ jpopulator-core/src/main/java/io/github/benas/jpopulator/impl/ContextStackItem.java
@@ -0,0 +1,59 @@
+/*
+ * The MIT License
+ *
+ *   Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package io.github.benas.jpopulator.impl;
+
+
+import java.lang.reflect.Field;
+
+/**
+ * An object holding data about the recursion step of {@link io.github.benas.jpopulator.api.Populator}
+ *
+ * @author Rémi Alvergnat (toilal.dev@gmail.com)
+ */
+public class ContextStackItem {
+    private Object object;
+    private Field field;
+
+    public ContextStackItem(Object object, Field field) {
+        this.object = object;
+        this.field = field;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+}
