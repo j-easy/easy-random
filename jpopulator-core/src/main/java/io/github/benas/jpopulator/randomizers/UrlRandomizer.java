7167adf8ba35df431d1619f8070e8db00739a83f
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2015-12-12 21:27:55 +0100

    update license

--- jpopulator-core/src/main/java/io/github/benas/jpopulator/randomizers/UrlRandomizer.java
+++ jpopulator-core/src/main/java/io/github/benas/jpopulator/randomizers/UrlRandomizer.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- *   Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ *   Copyright (c) 2016, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,7 @@
  *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *   THE SOFTWARE.
+ *
  */
 
 package io.github.benas.jpopulator.randomizers;
