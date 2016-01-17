fe3dc4b35157d75efdc13454967d25b8b42e585d
Fred Eckertson <feckertson@cerner.com>
2016-01-17 07:30:21 -0600

    fix getRandomArray to handle primitives

---
+++ random-beans/src/test/java/io/github/benas/randombeans/beans/PrimitiveArrayBean.java
@@ -0,0 +1,42 @@
+package io.github.benas.randombeans.beans;
+
+/**
+ * Java bean used to test the population of collection classes.
+ *
+ * @author Fred Eckertson (feckertson@cerner.com)
+ */
+public class PrimitiveArrayBean {
+    byte[] byteArray;
+    short[] shortArray;
+    int[] intArray;
+    long[] longArray;
+    float[] floatArray;
+    double[] doubleArray;
+    boolean[] booleanArray;
+    char[] charArray;
+
+    public byte[] getByteArray() {
+        return byteArray;
+    }
+    public short[] getShortArray() {
+        return shortArray;
+    }
+    public int[] getIntArray() {
+        return intArray;
+    }
+    public long[] getLongArray() {
+        return longArray;
+    }
+    public float[] getFloatArray() {
+        return floatArray;
+    }
+    public double[] getDoubleArray() {
+        return doubleArray;
+    }
+    public char[] getCharArray() {
+        return charArray;
+    }
+    public boolean[] getBooleanArray() {
+        return booleanArray;
+    }
+}
