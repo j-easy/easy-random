f4753ef2479785ad0532722399889fa722d8a1c7
Pascal Schumacher <pascalschumacher@gmx.net>
2016-02-08 22:21:44 +0100

    pretty-print some test beans

--- random-beans/src/test/java/io/github/benas/randombeans/beans/B.java
+++ random-beans/src/test/java/io/github/benas/randombeans/beans/B.java
@@ -1,22 +1,23 @@
 package io.github.benas.randombeans.beans;
 
 public class B {
-        private A a1;
-        private A a2;
 
-        public A getA1() {
-            return a1;
-        }
+    private A a1;
+    private A a2;
 
-        public void setA1(A a1) {
-            this.a1 = a1;
-        }
-
-        public A getA2() {
-            return a2;
-        }
-
-        public void setA2(A a2) {
-            this.a2 = a2;
-        }
+    public A getA1() {
+        return a1;
     }
+
+    public void setA1(A a1) {
+        this.a1 = a1;
+    }
+
+    public A getA2() {
+        return a2;
+    }
+
+    public void setA2(A a2) {
+        this.a2 = a2;
+    }
+}
