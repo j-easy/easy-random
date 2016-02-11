721fd96f6f887fa3657efc02e2c0beaaf60b994f
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-02-11 10:46:46 +0100

    update license header

--- random-beans/src/test/java/io/github/benas/randombeans/beans/WildCardCollectionBean.java
+++ random-beans/src/test/java/io/github/benas/randombeans/beans/WildCardCollectionBean.java
@@ -3,11 +3,11 @@
  *
  *   Copyright (c) 2016, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
  *
- *   Permission is hereby granted, free of charge, to any ? extends Runnable obtaining a copy
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
  *   in the Software without restriction, including without limitation the rights
  *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit ? extends Runnables to whom the Software is
+ *   copies of the Software, and to permit persons to whom the Software is
  *   furnished to do so, subject to the following conditions:
  *
  *   The above copyright notice and this permission notice shall be included in
