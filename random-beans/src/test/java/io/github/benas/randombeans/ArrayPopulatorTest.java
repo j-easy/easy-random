cbe26af766dca4319262222859efc8dcf7698291
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-02-13 22:51:12 +0100

    fix primitive array population

---
+++ random-beans/src/test/java/io/github/benas/randombeans/ArrayPopulatorTest.java
@@ -0,0 +1,80 @@
+/*
+ * The MIT License
+ *
+ *    Copyright (c) 2016, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in
+ *    all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *    THE SOFTWARE.
+ */
+
+package io.github.benas.randombeans;
+
+import io.github.benas.randombeans.api.Populator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyByte;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ArrayPopulatorTest {
+
+    public static final int INT = 10;
+    public static final String STRING = "FOO";
+
+    @Mock
+    private Populator populator;
+
+    private ArrayPopulator arrayPopulator;
+
+    @Before
+    public void setUp() throws Exception {
+        arrayPopulator = new ArrayPopulator(populator);
+        when(populator.populate(Integer.TYPE)).thenReturn(INT);
+        when(populator.populate(String.class)).thenReturn(STRING);
+        when(populator.populate(any(Class.class), anyByte())).thenReturn(singletonList(STRING));
+    }
+
+    @Test
+    public void getRandomArray() throws Exception {
+        Object array = arrayPopulator.getRandomArray(String[].class);
+
+        assertThat(array).isInstanceOf(String[].class);
+        String[] strings = (String[]) array;
+        for (String string : strings) {
+            assertThat(string).isEqualTo(STRING);
+        }
+    }
+
+    @Test
+    public void getRandomPrimitiveArray() throws Exception {
+        Object array = arrayPopulator.getRandomPrimitiveArray(Integer.TYPE);
+
+        assertThat(array).isInstanceOf(int[].class);
+        int[] ints = (int[]) array;
+        for (int i : ints) {
+            assertThat(i).isEqualTo(INT);
+        }
+    }
+}
