736c697612fdac1a989b78cf41cc020c665d8c3c
Pascal Schumacher <pascalschumacher@gmx.net>
2016-04-10 10:38:10 +0200

    improve test coverage of some classes of the time package

---
+++ random-beans/src/test/java/io/github/benas/randombeans/randomizers/time/TimeRandomizersTest.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License
+ *
+ *   Copyright (c) 2016, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package io.github.benas.randombeans.randomizers.time;
+
+import static io.github.benas.randombeans.randomizers.time.DurationRandomizer.aNewDurationRandomizer;
+import static io.github.benas.randombeans.randomizers.time.LocalDateRandomizer.aNewLocalDateRandomizer;
+import static io.github.benas.randombeans.randomizers.time.LocalTimeRandomizer.aNewLocalTimeRandomizer;
+import static io.github.benas.randombeans.randomizers.time.MonthDayRandomizer.aNewMonthDayRandomizer;
+import static io.github.benas.randombeans.randomizers.time.PeriodRandomizer.aNewPeriodRandomizer;
+import static io.github.benas.randombeans.randomizers.time.YearMonthRandomizer.aNewYearMonthRandomizer;
+import static io.github.benas.randombeans.randomizers.time.YearRandomizer.aNewYearRandomizer;
+import static io.github.benas.randombeans.randomizers.time.ZoneOffsetRandomizer.aNewZoneOffsetRandomizer;
+import static org.assertj.core.api.BDDAssertions.then;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.Period;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+import io.github.benas.randombeans.api.Randomizer;
+import io.github.benas.randombeans.randomizers.AbstractRandomizerTest;
+
+@RunWith(DataProviderRunner.class)
+public class TimeRandomizersTest extends AbstractRandomizerTest<Randomizer<?>> {
+
+    @DataProvider
+    public static Object[] generateRandomizers() {
+        return new Object[] {
+                aNewDurationRandomizer(),
+                aNewLocalDateRandomizer(),
+                aNewMonthDayRandomizer(),
+                aNewLocalTimeRandomizer(),
+                aNewPeriodRandomizer(),
+                aNewYearRandomizer(),
+                aNewYearMonthRandomizer(),
+                aNewZoneOffsetRandomizer(),
+        };
+    }
+
+    @Test
+    @UseDataProvider("generateRandomizers")
+    public void generatedTimeShouldNotBeNull(Randomizer<?> randomizer) {
+        // when
+        Object randomNumber = randomizer.getRandomValue();
+
+        then(randomNumber).isNotNull();
+    }
+
+    @DataProvider
+    public static Object[][] generateSeededRandomizersAndTheirExpectedValues() {
+        return new Object[][] {
+                { aNewDurationRandomizer(SEED), Duration.of(72L, ChronoUnit.HOURS) },
+                { aNewLocalDateRandomizer(SEED), LocalDate.of(2020, Month.MARCH, 20) },
+                { aNewMonthDayRandomizer(SEED), MonthDay.of(Month.MARCH, 20) },
+                { aNewLocalTimeRandomizer(SEED), LocalTime.of(16, 42, 58) },
+                { aNewPeriodRandomizer(SEED), Period.of(2020, 3, 20) },
+                { aNewYearRandomizer(SEED), Year.of(2020) },
+                { aNewYearMonthRandomizer(SEED), YearMonth.of(2020, Month.MARCH) },
+                { aNewZoneOffsetRandomizer(SEED), ZoneOffset.ofTotalSeconds(28923) },
+        };
+    }
+
+    @Test
+    @UseDataProvider("generateSeededRandomizersAndTheirExpectedValues")
+    public void shouldGenerateTheSameValueForTheSameSeed(Randomizer<?> randomizer, Object expected) {
+        //when
+        Object actual = randomizer.getRandomValue();
+
+        then(actual).isEqualTo(expected);
+    }
+}
