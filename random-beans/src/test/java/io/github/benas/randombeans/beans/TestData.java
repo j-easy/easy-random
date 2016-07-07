2d171c934057f6782e946bf6a28e548996492950
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-07-07 12:41:51 +0200

    add time precision in test case of date conversion from randomizer constructor argument annotation
    The build was unstable because of failing test:
    
    Failed tests:
      EnhancedRandomImplTest.annotatedRandomizerArgumentsShouldBeCorrectlyParsed:420
    Expecting:
     <2016-01-30T00:19:49.572>
    to be in period:
     [2016-01-10T00:00:00.000, 2016-01-30T00:00:00.000[

--- random-beans/src/test/java/io/github/benas/randombeans/beans/TestData.java
+++ random-beans/src/test/java/io/github/benas/randombeans/beans/TestData.java
@@ -36,8 +36,8 @@ public class TestData {
     }
 
     @Randomizer(value = DateRangeRandomizer.class, args = {
-            @RandomizerArgument(value = "2016-01-10", type = Date.class),
-            @RandomizerArgument(value = "2016-01-30", type = Date.class)
+            @RandomizerArgument(value = "2016-01-10T00:00:00", type = Date.class),
+            @RandomizerArgument(value = "2016-01-30T59:59:59", type = Date.class)
     })
     private Date date;
 
