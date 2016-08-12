5aedcf9ec76574a366aed78ee42d1ceb92e1ff0e
Dovid Kopel <dovid@dovidkopel.com>
2016-08-12 18:25:18 +0100

    Added unit test

--- random-beans/src/test/java/io/github/benas/randombeans/ObjectFactoryTest.java
+++ random-beans/src/test/java/io/github/benas/randombeans/ObjectFactoryTest.java
@@ -23,9 +23,21 @@
  */
 package io.github.benas.randombeans;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.benas.randombeans.api.ProxyRegistry;
+import io.github.benas.randombeans.beans.Person;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.ClassFileVersion;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.DelayQueue;
@@ -73,6 +85,46 @@ public class ObjectFactoryTest {
     @Test(expected = UnsupportedOperationException.class)
     public void delayQueueShouldBeRejected() {
         objectFactory.createEmptyCollectionForType(DelayQueue.class, INITIAL_CAPACITY);
+    }
+
+    private String name = null;
+
+    public class NameC {
+        public String getEmail() {
+            return name;
+        }
+    }
+
+    @Test
+    public void proxyTest() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ProxyRegistry proxyRegistry = new ProxyRegistryImpl();
+        name = "tom";
+        DynamicType.Unloaded<? extends Person> bc = new ByteBuddy(ClassFileVersion.JAVA_V8)
+                .subclass(Person.class)
+                .method(ElementMatchers.named("getName"))
+                .intercept(MethodDelegation.to(new NameC()))
+                .make();
+        proxyRegistry.proxy(Person.class, bc);
+        objectFactory = new ObjectFactory(proxyRegistry);
+
+        Person b = objectFactory.createInstance(Person.class);
+
+        b.setName("ronald");
+
+        Assert.assertEquals(name, b.getName());
+
+        String bs = objectMapper.writeValueAsString(b);
+
+        Class<? extends Person> bbc = bc.load(
+                getClass().getClassLoader(),
+                ClassLoadingStrategy.Default.WRAPPER
+        ).getLoaded();
+        Person bb = objectMapper.readValue(bs, bbc);
+        name = "john";
+
+        bb.setName("donald");
+        Assert.assertEquals(name, bb.getName());
     }
 
     private abstract class AbstractFoo {
